8766c5a0fcd8b1489b68468c23aea8938b736bcf
proficientcoder <krakkus@outlook.com>
2021-02-14 19:31:02 +0100

    .

--- contract/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-
-from contract import views
-
-urlpatterns = [
-    path('register/', views.register, name='contract_create'),
-    path('', views.contract, name='contract_'),
-]
